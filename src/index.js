3fef1772bbfd2254e0c560f94a161a1b334be27a
MarcusAnglin <21136754+MarcusAnglin@users.noreply.github.com>
2022-06-08 00:08:08 -0400

    no major changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { AppProvider } from './context'
-import { BrowserRouter as Router } from 'react-router-dom'
-ReactDOM.render(
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { AppProvider } from "./context";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <AppProvider>
       <App />
     </AppProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
