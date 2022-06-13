cffe41a9cabae966becc62b31b187d7d4be807de
MarcusAnglin <21136754+MarcusAnglin@users.noreply.github.com>
2022-06-13 09:13:34 -0400

    completed single movie component

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,9 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
-import { API_ENDPOINT } from './context'
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { API_ENDPOINT } from "./context";
+
+const notFoundUrl =
+  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const SingleMovie = () => {
-  return <h2>single movie</h2>
-}
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState({ show: false, message: "" });
 
-export default SingleMovie
+  const fetchMovie = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    if (data.Response === "False") {
+      setError({ show: true, message: data.Error });
+    } else {
+      setMovie(data);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovie(`${API_ENDPOINT}&i=${id}`);
+  }, [id]);
+
+  if (loading) {
+    return <div className="loading"></div>;
+  }
+
+  if (error.show) {
+    return (
+      <div className="page-error">
+        <h1>{error.message}</h1>
+        <Link to="/" className="btn">
+          back to movies
+        </Link>
+      </div>
+    );
+  }
+
+  const { Poster: poster, Title: title, Plot: plot, Year: year } = movie;
+
+  return (
+    <section className="single-movie">
+      <img src={poster === "N/A" ? notFoundUrl : poster} alt={title} />
+      <div className="single-movie-info">
+        <h2>{title}</h2>
+        <p>{plot}</p>
+        <h4>{year}</h4>
+        <Link to="/" className="btn">
+          back to movies
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default SingleMovie;
